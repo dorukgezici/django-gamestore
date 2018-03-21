52c08537ab3783143268ab5c86b6247455e4ac32
Louis SUGY <louis.sugy@insa-lyon.fr>
2018-03-21 20:03:06 +0200

    Added dummy view

--- gamestore/views.py
+++ gamestore/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def dummy_view(request):
+    return HttpResponse("This is the dummy Heroku deployment for group g-056 (Django Reinhardt).", content_type="text/plain")
--- wsd2018project/urls.py
+++ wsd2018project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import gamestore.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', gamestore.views.dummy_view)
 ]
